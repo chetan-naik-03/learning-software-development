cc64313ac639dded0dc32047f0cbf9e70cec2dd2
Chetan Naik <cnaik7940@gmail.com>
2025-10-07 21:06:02 +0530

    lesson 02

---
+++ week-01/2 numbers sum.py
@@ -0,0 +1,13 @@
+#Pactice quetions#
+print("Hello world")
+
+#Write a program to input 2 numbers and print their sum
+first = int(input("enter first= "))
+second = int(input("enter second= "))
+
+print("sum =", first + second)
+
+#WAP to input side of a square & print its area.
+side = float(input("enter square side ="))
+
+print("area=", side*side)
---
+++ week-01/Average.py
@@ -0,0 +1,5 @@
+#WAP to input 2 floating  point numbers & print their average
+a = float(input("enter first = "))
+b = float(input("enter second = "))
+
+print("avg=", (a+b)/2)
